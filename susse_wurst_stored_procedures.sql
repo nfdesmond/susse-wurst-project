57cebf051d91e352718d27782ceaa8ec57fd2ddd
nfdesmond <nfdesmond@duck.com>
2025-10-31 11:20:52 -0700

    Added employee onboarding stored procedure, tested in MySQL.

--- susse_wurst_stored_procedures.sql
+++ susse_wurst_stored_procedures.sql
@@ -138,3 +138,126 @@ BEGIN
 END//
 
 DELIMITER ;
+
+
+
+-- HR CRUD procedures
+
+DROP PROCEDURE IF EXISTS sp_employee_insert;
+DROP PROCEDURE IF EXISTS sp_emp_onboard;
+
+DELIMITER //
+
+-- INSERT INTO EMPLOYEE
+CREATE PROCEDURE sp_employee_insert
+(
+	IN id_var INT,
+    IN fname_var VARCHAR(75),
+    IN lname_var VARCHAR(75),
+    IN hiredate_var DATE,
+    IN dob_var DATE,
+    IN ssn_var CHAR(11),
+    IN address_var VARCHAR(75),
+    IN city_var VARCHAR(75),
+    IN state_var CHAR(2),
+    IN zip_var CHAR(5),
+    IN phone_var CHAR(12),
+    OUT emp_insert_msg BOOL
+)
+COMMENT 'This procedure inserts a new employee into the EMPLOYEE table.'
+
+BEGIN
+	DECLARE insert_error BOOL DEFAULT FALSE;
+    
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+		SET insert_error = TRUE;
+    
+        
+	START TRANSACTION;
+		INSERT INTO employee
+        (emp_id, emp_fname, emp_lname, emp_hire_date, emp_dob, emp_ssn,
+         emp_address, emp_city, emp_state, emp_zip, emp_phone)
+		VALUES (id_var, fname_var, lname_var, hiredate_var, dob_var, ssn_var,
+                       address_var, city_var, state_var, zip_var, phone_var);
+		
+        IF insert_error = FALSE THEN
+			COMMIT;
+            SET emp_insert_msg = TRUE;
+		ELSE
+			ROLLBACK;
+            SET emp_insert_msg = FALSE;
+		END IF;
+END//
+
+-- INSERT INTO ACTIVE EMPLOYEE
+CREATE PROCEDURE IF NOT EXISTS sp_active_emp_insert
+(
+    IN empid_var INT, 
+    IN jobid_var TINYINT,
+    IN deptid_var TINYINT,
+    IN empcomp_var DECIMAL(8,2),
+    IN email_var VARCHAR(100),
+    OUT active_emp_insert_msg BOOL 
+)
+COMMENT 'This procedure inserts a new employee into the ACTIVE_EMPLOYEE table.'
+
+BEGIN
+    DECLARE insert_error BOOL DEFAULT FALSE;
+
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+        SET insert_error = TRUE;
+
+    START TRANSACTION;
+        INSERT INTO active_employee
+            (emp_id, position_id, dept_id, active_emp_comp, active_emp_email)
+        VALUES (empid_var, jobid_var, deptid_var, empcomp_var, email_var);
+
+        IF insert_error = FALSE THEN
+			COMMIT;
+            SET active_emp_insert_msg = TRUE;
+		ELSE
+			ROLLBACK;
+            SET active_emp_insert_msg = FALSE;
+		END IF;
+END//
+
+
+-- BUNDLED ONBOARDING PROCEDURE
+CREATE PROCEDURE sp_emp_onboard
+(
+	IN id_var INT,
+    IN fname_var VARCHAR(75),
+    IN lname_var VARCHAR(75),
+    IN hiredate_var DATE,
+    IN dob_var DATE,
+    IN ssn_var CHAR(11),
+    IN address_var VARCHAR(75),
+    IN city_var VARCHAR(75),
+    IN state_var CHAR(2),
+    IN zip_var CHAR(5),
+    IN phone_var CHAR(12),
+    IN jobid_var TINYINT,
+    IN deptid_var TINYINT,
+    IN empcomp_var DECIMAL(8,2),
+    IN email_var VARCHAR(100)
+)
+COMMENT 'This procedure inserts a new employee into the EMPLOYEE and ACTIVE_EMPLOYEE tables.'
+
+BEGIN
+    CALL sp_employee_insert(
+        id_var, fname_var, lname_var, hiredate_var, dob_var, ssn_var,
+        address_var, city_var, state_var, zip_var, phone_var, @emp_insert_msg
+    );
+
+    CALL sp_active_emp_insert(
+        id_var, jobid_var, deptid_var, empcomp_var, email_var, @active_emp_insert_msg
+    );
+
+    IF @emp_insert_msg = TRUE AND @active_emp_insert_msg THEN
+        SELECT 'New employee onboarded successfully.' AS "MESSAGE";
+    ELSE
+        SELECT 'An error occurred. The transaction was not completed.' AS "UPDATE";
+	END IF;
+END//
+
+DELIMITER ;
