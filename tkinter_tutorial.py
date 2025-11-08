ffd092077a5d72b5e4602b9a8f2409cef2113f18
nfdesmond <nfdesmond@duck.com>
2025-11-08 13:31:37 -0700

    Updated import statements to remove ttk import (not needed).

--- tkinter_tutorial.py
+++ tkinter_tutorial.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import ttk
 import sw_db_application.sussewurstconnect.swconnect as swconnect
 import sw_db_application.sussewurstconnect.swconfig as swconfig
 from employeelookupgui import EmployeeLookupGUI
