2e53435384433c28b9c6cbbecc3d7d584aaf373b
nfdesmond <nfdesmond@duck.com>
2025-11-12 14:48:58 -0700

    Starting the onboarding GUI

--- tkinter_tutorial.py
+++ tkinter_tutorial.py
@@ -4,6 +4,7 @@ import sw_db_application.sussewurstconnect.swconfig as swconfig
 from employeelookupgui import EmployeeLookupGUI
 from storelookupgui import StoreLookupGUI
 from onboardgui import EmployeeOnboardGUI
+
 hr_app = swconnect.SusseWurstConnect('susse_wurst_hr')
 
 hr_cxn = hr_app.mysql_connect(swconfig.USER, swconfig.PSWD, use_pure_flag=True)
@@ -11,9 +12,9 @@ hr_cxn = hr_app.mysql_connect(swconfig.USER, swconfig.PSWD, use_pure_flag=True)
 if hr_app.cxn_test(hr_cxn):
 
     root = tk.Tk()
-    # EmployeeLookupGUI(root, hr_cxn)
+    EmployeeLookupGUI(root, hr_cxn)
     # StoreLookupGUI(root, hr_cxn)
-    EmployeeOnboardGUI(root, hr_cxn)
+    # EmployeeOnboardGUI(root, hr_cxn)
     
     root.mainloop()
     
